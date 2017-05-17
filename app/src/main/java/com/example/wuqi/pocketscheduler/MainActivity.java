8260a010b95beb68e9eb0a86b2e909e424ce609a
RaphaeltheKid <Raphael9312@gmail.com>
2017-05-17 16:25:50 -0500

    test for commit and push

--- app/src/main/java/com/example/wuqi/pocketscheduler/MainActivity.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         int i = 9;
+        String nantian;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
