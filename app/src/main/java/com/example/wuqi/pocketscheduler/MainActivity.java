b91a9b1434664c6b237c8ec771d7745114a8cb9e
underbringer <underbringer@163.com>
2017-05-17 16:29:11 -0500

    restore to original

--- app/src/main/java/com/example/wuqi/pocketscheduler/MainActivity.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/MainActivity.java
@@ -7,7 +7,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int i = 9;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
