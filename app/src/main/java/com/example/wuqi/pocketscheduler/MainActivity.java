fd4c4a0f4b8c21d1415c333a59745174da938d34
underbringer <underbringer@163.com>
2017-05-17 16:07:42 -0500

    test for git

--- app/src/main/java/com/example/wuqi/pocketscheduler/MainActivity.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int i = 9;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
