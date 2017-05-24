6aa92cd1d8928cbbdf8cd475a795ab5a12721510
Qi Wu <wuqicn57@gmail.com>
2017-05-24 16:41:43 -0500

    set EventFragment layout, contains bug.  cannot open event fragment

--- app/src/main/java/com/example/wuqi/pocketscheduler/EventComingSoonFragment.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/EventComingSoonFragment.java
@@ -4,24 +4,21 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import java.lang.reflect.Field;
-
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link EventsFragment.OnFragmentInteractionListener} interface
+ * {@link EventComingSoonFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link EventsFragment#newInstance} factory method to
+ * Use the {@link EventComingSoonFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EventsFragment extends Fragment {
+public class EventComingSoonFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -31,15 +28,9 @@ public class EventsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    // Button
-    private Button mTabEvent_ComingSoon;
-    private Button mTabEvent_Draftbox;
-    private Button mTabEvent_Pendingbox;
-    private Button mTabEvent_Previous;
-
     private OnFragmentInteractionListener mListener;
 
-    public EventsFragment() {
+    public EventComingSoonFragment() {
         // Required empty public constructor
     }
 
@@ -49,11 +40,11 @@ public class EventsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EventsFragment.
+     * @return A new instance of fragment EventComingSoonFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EventsFragment newInstance(String param1, String param2) {
-        EventsFragment fragment = new EventsFragment();
+    public static EventComingSoonFragment newInstance(String param1, String param2) {
+        EventComingSoonFragment fragment = new EventComingSoonFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -73,54 +64,9 @@ public class EventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View ra = inflater.inflate(R.layout.fragment_events, container, false);
-        mTabEvent_ComingSoon = (Button) ra.findViewById(R.id.Button_ComingSoon);
-        mTabEvent_Draftbox = (Button) ra.findViewById(R.id.Button_DraftBox);
-        mTabEvent_Pendingbox = (Button) ra.findViewById(R.id.Button_PendingBox);
-        mTabEvent_Previous = (Button) ra.findViewById(R.id.Button_Previous);
-
-        mTabEvent_Previous.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Fragment fm = new EventPreviousFragment();
-                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-                transaction.replace(R.id.event_frame, fm).addToBackStack(null).commit();
-            }
-        });
-
-        mTabEvent_ComingSoon.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Fragment fm = new EventComingSoonFragment();
-                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-                transaction.replace(R.id.event_frame, fm).addToBackStack(null).commit();
-            }
-        });
-
-        mTabEvent_Draftbox.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Fragment fm = new EventDraftboxFragment();
-                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-                transaction.replace(R.id.event_frame, fm).addToBackStack(null).commit();
-            }
-        });
-
-        mTabEvent_Pendingbox.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Fragment fm = new EventPendingboxFragment();
-                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-                transaction.replace(R.id.event_frame, fm).addToBackStack(null).commit();
-            }
-        });
-        setDefaultFragment();
-        return ra;
+        return inflater.inflate(R.layout.fragment_events_comingsoon, container, false);
     }
 
-    private void setDefaultFragment(){
-        Fragment fm = new EventPreviousFragment();
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        transaction.replace(R.id.event_frame, fm);
-        transaction.addToBackStack(null).commit();
-    }
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
