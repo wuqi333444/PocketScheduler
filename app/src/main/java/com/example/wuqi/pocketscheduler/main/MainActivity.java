0e365d69bdc3cacb6e0cad2d306e63ce34a762cb
Qi Wu <wuqicn57@gmail.com>
2017-05-25 16:54:46 -0500

    add right side menu

--- app/src/main/java/com/example/wuqi/pocketscheduler/main/MainActivity.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -39,6 +40,11 @@ public class MainActivity extends AppCompatActivity implements CalendarFragment.
         setContentView(R.layout.activity_main);
         setupBottomTab();
         setDefaultFragment();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
     private void setDefaultFragment() {
         android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
