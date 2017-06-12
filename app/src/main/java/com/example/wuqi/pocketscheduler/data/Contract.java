f373af29fa7af27b9484af4073532b31cde1cfe3
RaphaeltheKid <Raphael9312@gmail.com>
2017-06-11 21:55:52 -0500

    finish Contract and PocketDBHelper

--- app/src/main/java/com/example/wuqi/pocketscheduler/data/Contract.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/data/Contract.java
@@ -7,17 +7,86 @@ import android.provider.BaseColumns;
  */
 
 public final class Contract {
-    public static abstract class PetsEntry implements BaseColumns {
-        public static final String TABLE_NAME = "pets";
+    public static abstract class UserEntry implements BaseColumns {
+        public static final String TABLE_NAME = "user";
 
         public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_BREED = "breed";
-        public static final String COLUMN_GENDER = "gender";
-        public static final String COLUMN_WEIGHT = "weight";
+        public static final String COLUMN_USERID = "userid";
+    }
 
-        public static final int GENDER_MALE = 1;
-        public static final int GENDER_FEMALE = 2;
-        public static final int GENDER_UNKNOWN = 0;
+    public static abstract class ProjectEntry implements BaseColumns{
+        public static final String TABLE_NAME = "project";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PROJECTID = "projectid";
+        public static final String COLUMN_CREATOR = "creator";
+        public static final String COLUMN_BEGINTIME = "begintime";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLLUMN_DESCRIPTION = "description";
+    }
+
+    public static abstract class EventEntry implements BaseColumns{
+        public static final String TABLE_NAME = "event";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_EVENTID = "eventid";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_STARTTIME = "starttime";
+        public static final String COLUMN_PROJECTID = "projectid";
+        public static final String COLUMN_ENDTIME = "endtime";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_ISLONG = "islong";
+        public static final String COLUMN_EDITPRIORITY = "editpriority";
+        public static final String COLUMN_TYPE = "type";
+        public static final String COLUMN_ISFINISHED = "isfinished";
+        public static final String COLUMN_ISSHARED = "isshared";
+        public static final String COLUMN_CREATORID = "creatorid";
+        public static final String COLUMN_LOCATION = "location";
+
+        public static final int TYPE_ENTERTAIMENT = 1;
+        public static final int TYPE_STUDY = 2;
+        public static final int TYPE_JOB = 3;
+        public static final int TYPE_FRIEND = 4;
+        public static final int TYPE_FAMILY = 5;
+        public static final int TYPE_TRIP = 6;
+
+        public static final int EDITPRIORITY_CREATOR = 0;
+        public static final int EDITPRIORITY_ALL = 1;
+    }
+
+    public static abstract class DraftboxEntry extends EventEntry{
+    }
+
+    public static abstract class MemberEntry implements BaseColumns{
+        public static final String TABLE_NAME = "member";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_MEMBERID = "memberid";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_ISFRIEND = "isfriend";
+    }
+
+    public static abstract class Project_Members_pairEntry implements BaseColumns{
+        public static final String TABLE_NAME = "project_members_pair";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PROJECTID = "projectid";
+        public static final String COLUMN_MEMBERID = "memberid";
+    }
+
+    public static abstract class Event_Candidates_pairEntry implements BaseColumns{
+        public static final String TABLE_NAME = "event_candidates_pair";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_EVENTID = "eventid";
+        public static final String COLUMN_CANDIDATEID = "candidateid";
+    }
+
+    public static abstract class Event_Editable_pairEntry implements BaseColumns{
+        public static final String TABLE_NAME = "event_editable_pair";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_EVENTID = "eventid";
+        public static final String COLUMN_EDITABLEID = "editableid";
     }
 }
--- app/src/main/java/com/example/wuqi/pocketscheduler/data/PocketDBHelper.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/data/PocketDBHelper.java
@@ -3,7 +3,7 @@ package com.example.wuqi.pocketscheduler.data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import com.example.wuqi.pocketscheduler.data.Contract.PetsEntry;
+import com.example.wuqi.pocketscheduler.data.Contract;
 
 /**
  * Created by RapHaeL on 2017/6/4.
@@ -15,22 +15,111 @@ public class PocketDBHelper extends SQLiteOpenHelper {
     public PocketDBHelper(Context context){
         super(context,DATABASE_NAME,null,DATABASE_VERSION);
     }
-    private static final String SQL_CREATE_ENTRIES =
-            "CREATE TABLE " + PetsEntry.TABLE_NAME + " ("
-                    + PetsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + PetsEntry.COLUMN_NAME + " TEXT NOT NULL,"
-                    + PetsEntry.COLUMN_BREED + " TEXT,"
-                    + PetsEntry.COLUMN_GENDER + " INTEGER NOT NULL,"
-                    + PetsEntry.COLUMN_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
 
+    private static final String SQL_CREATE_USERENTRY =
+            "CREATE TABLE " + Contract.UserEntry.TABLE_NAME + " ("
+                    + Contract.UserEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.UserEntry.COLUMN_USERID + " INTEGER NOT NULL);";
+
+    private static final String SQL_CREATE_PROJECTENTRY =
+            "CREATE TABLE " + Contract.ProjectEntry.TABLE_NAME + " ("
+                    + Contract.ProjectEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.ProjectEntry.COLUMN_PROJECTID + " INTEGER NOT NULL,"
+                    + Contract.ProjectEntry.COLLUMN_DESCRIPTION + " TEXT,"
+                    + Contract.ProjectEntry.COLUMN_BEGINTIME + " TEXT,"
+                    + Contract.ProjectEntry.COLUMN_CREATOR + " TEXT,"
+                    + Contract.ProjectEntry.COLUMN_TITLE + " TEXT);";
+
+    private static final String SQL_CREATE_EVENTENTRY =
+            "CREATE TABLE " + Contract.EventEntry.TABLE_NAME + " ("
+                    + Contract.EventEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.EventEntry.COLUMN_EVENTID + " INTEGER NOT NULL,"
+                    + Contract.EventEntry.COLUMN_TITLE + " TEXT NOT NULL,"
+                    + Contract.EventEntry.COLUMN_STARTTIME + " INTEGER NOT NULL,"
+                    + Contract.EventEntry.COLUMN_ENDTIME + " INTEGER,"
+                    + Contract.EventEntry.COLUMN_PROJECTID + " TEXT,"
+                    + Contract.EventEntry.COLUMN_DESCRIPTION + " TEXT,"
+                    + Contract.EventEntry.COLUMN_CREATORID + " INTEGER NOT NULL,"
+                    + Contract.EventEntry.COLUMN_ISFINISHED + " BOOLEAN,"
+                    + Contract.EventEntry.COLUMN_ISLONG + " BOOLEAN,"
+                    + Contract.EventEntry.COLUMN_ISSHARED + " BOOLEAN,"
+                    + Contract.EventEntry.COLUMN_TYPE + " INTEGER,"
+                    + Contract.EventEntry.COLUMN_EDITPRIORITY + " INTEGER,"
+                    + Contract.EventEntry.COLUMN_LOCATION + " TEXT);";
+
+    private static final String SQL_CREATE_DRAFTBOXENTRY =
+            "CREATE TABLE " + Contract.DraftboxEntry.TABLE_NAME + " ("
+                    + Contract.DraftboxEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.DraftboxEntry.COLUMN_EVENTID + " INTEGER NOT NULL,"
+                    + Contract.DraftboxEntry.COLUMN_TITLE + " TEXT NOT NULL,"
+                    + Contract.DraftboxEntry.COLUMN_STARTTIME + " INTEGER NOT NULL,"
+                    + Contract.DraftboxEntry.COLUMN_ENDTIME + " INTEGER,"
+                    + Contract.DraftboxEntry.COLUMN_PROJECTID + " TEXT,"
+                    + Contract.DraftboxEntry.COLUMN_DESCRIPTION + " TEXT,"
+                    + Contract.DraftboxEntry.COLUMN_CREATORID + " INTEGER NOT NULL,"
+                    + Contract.DraftboxEntry.COLUMN_ISFINISHED + " BOOLEAN,"
+                    + Contract.DraftboxEntry.COLUMN_ISLONG + " BOOLEAN,"
+                    + Contract.DraftboxEntry.COLUMN_ISSHARED + " BOOLEAN,"
+                    + Contract.DraftboxEntry.COLUMN_TYPE + " INTEGER,"
+                    + Contract.DraftboxEntry.COLUMN_EDITPRIORITY + " INTEGER,"
+                    + Contract.DraftboxEntry.COLUMN_LOCATION + " TEXT);";
+
+    private static final String SQL_CREATE_MEMBERENTRY =
+            "CREATE TABLE " + Contract.MemberEntry.TABLE_NAME + " ("
+                    + Contract.MemberEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.MemberEntry.COLUMN_MEMBERID + " INTEGER NOT NULL,"
+                    + Contract.MemberEntry.COLUMN_NAME + " TEXT,"
+                    + Contract.MemberEntry.COLUMN_ISFRIEND + " BOOLEAN);";
+
+    private static final String SQL_CREATE_PROJECT_MEMBERS_PAIRENTRY =
+            "CREATE TABLE " + Contract.Project_Members_pairEntry.TABLE_NAME + " ("
+                    + Contract.Project_Members_pairEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.Project_Members_pairEntry.COLUMN_PROJECTID + " INTEGER NOT NULL"
+                    + Contract.Project_Members_pairEntry.COLUMN_MEMBERID + "INTEGER NOT NULL);";
+
+    private static final String SQL_CREATE_EVENT_CANDIDATES_PAIRENTRY =
+            "CREATE TABLE " + Contract.Event_Candidates_pairEntry.TABLE_NAME + " ("
+                    + Contract.Event_Candidates_pairEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.Event_Candidates_pairEntry.COLUMN_EVENTID + " INTEGER NOT NULL"
+                    + Contract.Event_Candidates_pairEntry.COLUMN_CANDIDATEID + "INTEGER NOT NULL);";
+
+    private static final String SQL_CREATE_EVENT_EDITABLE_PAIRENTRY =
+            "CREATE TABLE " + Contract.Event_Editable_pairEntry.TABLE_NAME + " ("
+                    + Contract.Event_Editable_pairEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + Contract.Event_Editable_pairEntry.COLUMN_EVENTID + " INTEGER NOT NULL"
+                    + Contract.Event_Editable_pairEntry.COLUMN_EDITABLEID + "INTEGER NOT NULL);";
+
+    @Override
     public void onCreate(SQLiteDatabase db){
-        db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(SQL_CREATE_USERENTRY);
+        db.execSQL(SQL_CREATE_PROJECTENTRY);
+        db.execSQL(SQL_CREATE_EVENTENTRY);
+        db.execSQL(SQL_CREATE_DRAFTBOXENTRY);
+        db.execSQL(SQL_CREATE_MEMBERENTRY);
+        db.execSQL(SQL_CREATE_PROJECT_MEMBERS_PAIRENTRY);
+        db.execSQL(SQL_CREATE_EVENT_CANDIDATES_PAIRENTRY);
+        db.execSQL(SQL_CREATE_EVENT_EDITABLE_PAIRENTRY);
     }
 
-    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + PetsEntry.TABLE_NAME;
+    private static final String SQL_DELETE_USERENTRY = "DROP TABLE IF EXISTS " + Contract.UserEntry.TABLE_NAME;
+    private static final String SQL_DELETE_PROJECTENTRY = "DROP TABLE IF EXISTS " + Contract.ProjectEntry.TABLE_NAME;
+    private static final String SQL_DELETE_EVENTENTRY = "DROP TABLE IF EXISTS " + Contract.EventEntry.TABLE_NAME;
+    private static final String SQL_DELETE_DRAFTBOXENTRY = "DROP TABLE IF EXISTS " + Contract.DraftboxEntry.TABLE_NAME;
+    private static final String SQL_DELETE_MEMBERENTRY = "DROP TABLE IF EXISTS " + Contract.MemberEntry.TABLE_NAME;
+    private static final String SQL_DELETE_PROJECT_MEMBERS_PAIRENTRY = "DROP TABLE IF EXISTS " + Contract.Project_Members_pairEntry.TABLE_NAME;
+    private static final String SQL_DELETE_EVENT_CANDIDATES_PAIRENTRY = "DROP TABLE IF EXISTS " + Contract.Event_Candidates_pairEntry.TABLE_NAME;
+    private static final String SQL_DELETE_EVENT_EDITABLE_PAIRENTRY = "DROP TABLE IF EXISTS " + Contract.Event_Editable_pairEntry.TABLE_NAME;
 
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        db.execSQL(SQL_DELETE_ENTRIES);
+        db.execSQL(SQL_DELETE_USERENTRY);
+        db.execSQL(SQL_DELETE_PROJECTENTRY);
+        db.execSQL(SQL_DELETE_EVENTENTRY);
+        db.execSQL(SQL_DELETE_DRAFTBOXENTRY);
+        db.execSQL(SQL_DELETE_MEMBERENTRY);
+        db.execSQL(SQL_DELETE_PROJECT_MEMBERS_PAIRENTRY);
+        db.execSQL(SQL_DELETE_EVENT_CANDIDATES_PAIRENTRY);
+        db.execSQL(SQL_DELETE_EVENT_EDITABLE_PAIRENTRY);
         onCreate(db);
     }
 
