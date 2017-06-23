3ffbdb3a9481a4448ea13cbe4841ca07bea1a951
underbringer <underbringer@163.com>
2017-06-23 12:33:13 -0500

    left drawer is added. the listview of left drawer hasn't been filled so it's totally black now.

--- app/src/main/java/com/example/wuqi/pocketscheduler/main/MainActivity.java
+++ app/src/main/java/com/example/wuqi/pocketscheduler/main/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.wuqi.pocketscheduler.main;
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 import com.example.wuqi.pocketscheduler.calender.CalendarFragment;
@@ -31,6 +35,11 @@ public class MainActivity extends AppCompatActivity implements CalendarFragment.
     private ProjectFragment mProject;
     private EventsFragment mEvents;
     private TabLayout allTabs;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -43,6 +52,41 @@ public class MainActivity extends AppCompatActivity implements CalendarFragment.
         setDefaultFragment();
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                mDrawerLayout.openDrawer(mDrawerList);
+
+            }
+        });
+        // Set the adapter for the list view
+      //  mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+      //          R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+       // mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
